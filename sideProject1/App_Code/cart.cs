5f7140fe11aaac9c7000b43868bd7dd1e8aa19b6
Louisli8066 <a80665308@gmail.com>
2022-01-03 22:03:34 +0800

    ALL_20220103

---
+++ sideProject1/App_Code/cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for cart
+/// </summary>
+public class cart
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public int Price { get; set; }
+    public string ImageFileName { get; set; }
+    public int Quantity { get; set; }
+    public int Total { get { return Price * Quantity; } }
+}
